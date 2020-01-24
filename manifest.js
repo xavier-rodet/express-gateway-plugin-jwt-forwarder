d436861c594097fad88bd884650d2878ff4dcb90
Xavier RODET <xavier.rodet@gmail.com>
2020-01-24 11:31:26 +0100

    Release 0.0.6 Declare policy

--- manifest.js
+++ manifest.js
@@ -1,5 +1,6 @@
 module.exports = {
   version: '1.2.0',
+  policies: ['jwt-forwarder'],
   init: function(pluginContext) {
     let policy = require('./policies/jwt-forwarder-policy');
     pluginContext.registerPolicy(policy);
